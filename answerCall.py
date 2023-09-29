42f8738f8a6c55d90b8effb5b77aa317db4dc7cc
nathancaran <caran494@hsd.k12.or.us>
2023-09-29 09:42:43 -0700

    adding variables

--- answerCall.py
+++ answerCall.py
@@ -1,11 +1,15 @@
 # answerCall.py
 # by _______
+caller_codes = "U", "F", "R", "T"
+sameAreacode = "True", "False"
+cur_time = "09:15", "12:00", "14:00", "22:45", "00:30"
 
 # For instructions on what to do, see README.md
 # or visit (https://github.com/HundredVisionsGuy/answerCall)
 
 # Write function defintion: answerCall()
-
+def answerCall():
+    if 
 # Make sure it returns a value
 
 if __name__ == '__main__':
